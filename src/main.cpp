292f2dc5e2f9b03a059368af9f79f7329665ac41
unuecoolx <mr.sana259@gmail.com>
2021-07-31 17:12:51 +0300

    feat(project_structure): create project folder structure

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	PSTR lpCmdLine, INT nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hInstance);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(nCmdShow);
+	
+	return 0;
+}
