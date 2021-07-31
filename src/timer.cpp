b1816f3c9aa3ab0786516a21eb9961e55320e7a7
unuecoolx <mr.sana259@gmail.com>
2021-07-31 18:02:19 +0300

    feat(timer): add timer class for accurate time measurement

---
+++ src/timer.cpp
@@ -0,0 +1,83 @@
+#include "timer.hpp"
+
+Timer::Timer()
+{
+        _seconds_per_count = 0.0;
+        _delta_time = -1.0;
+        _base_time = 0;
+        _paused_time = 0;
+        _prev_time = 0;
+        _curr_time = 0;
+        _stopped = false;
+
+        int64_t counts_per_sec;
+        QueryPerformanceFrequency((LARGE_INTEGER*)&counts_per_sec);
+	_seconds_per_count = 1.0 / (double)counts_per_sec;
+}
+
+float Timer::total_time()
+{
+        if(_stopped)
+                return (float)(((_stop_time - _paused_time) - _base_time) * _seconds_per_count);
+        else
+                return (float)(((_curr_time - _paused_time) - _base_time) * _seconds_per_count);
+}
+
+float Timer::delta_time()
+{
+        return (float)(_delta_time);
+}
+
+void Timer::start()
+{
+        int64_t start_time;
+        QueryPerformanceCounter((LARGE_INTEGER*)&start_time);
+
+        if(_stopped) {
+                _paused_time += (start_time - _stop_time);
+
+                _prev_time = start_time;
+                _stop_time = 0;
+                _stopped = 0;
+        }
+}
+
+void Timer::stop()
+{
+        if(_stopped) {
+                int64_t curr_time;
+                QueryPerformanceCounter((LARGE_INTEGER*)&curr_time);
+
+                _stop_time = curr_time;
+                _stopped = true;
+        }
+}
+
+void Timer::reset()
+{
+        int64_t curr_time;
+        QueryPerformanceCounter((LARGE_INTEGER*)&curr_time);
+
+        _base_time = curr_time;
+        _prev_time = curr_time;
+        _stop_time = 0;
+        _stopped = false;
+}
+
+void Timer::tick()
+{
+        if(_stopped) {
+                _delta_time = 0.0;
+                return;
+        }
+
+        int64_t curr_time;
+        QueryPerformanceCounter((LARGE_INTEGER*)&curr_time);
+
+        _curr_time = curr_time;
+        _delta_time = (_curr_time - _prev_time) * _seconds_per_count;
+        _prev_time = _curr_time;
+
+        if(_delta_time < 0.0)
+                _delta_time = 0.0;
+}
---
+++ src/timer.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "strict_win.hpp"
+#include <cinttypes>
+
+class Timer 
+{
+public:
+        Timer();
+
+        float total_time();
+        float delta_time();
+
+        void start();
+        void stop();
+        void reset();
+        void tick();
+
+private:
+        double _seconds_per_count;
+        double _delta_time;
+
+        int64_t _base_time;
+        int64_t _paused_time;
+        int64_t _stop_time;
+        int64_t _prev_time;
+        int64_t _curr_time;
+
+        bool _stopped;
+};
