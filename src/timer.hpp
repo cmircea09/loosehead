84a8399b12e75588e84dd0f2c6d8f6566d96f457
unuecoolx <mr.sana259@gmail.com>
2021-08-01 20:52:18 +0300

    style(timer,window): convert indentation to tabs

--- src/timer.hpp
+++ src/timer.hpp
@@ -6,25 +6,25 @@
 class Timer 
 {
 public:
-        Timer();
+	Timer();
 
-        float total_time();
-        float delta_time();
+	float total_time();
+	float delta_time();
 
-        void start();
-        void stop();
-        void reset();
-        void tick();
+	void start();
+	void stop();
+	void reset();
+	void tick();
 
 private:
-        double _seconds_per_count;
-        double _delta_time;
+	double _seconds_per_count;
+	double _delta_time;
 
-        int64_t _base_time;
-        int64_t _paused_time;
-        int64_t _stop_time;
-        int64_t _prev_time;
-        int64_t _curr_time;
+	int64_t _base_time;
+	int64_t _paused_time;
+	int64_t _stop_time;
+	int64_t _prev_time;
+	int64_t _curr_time;
 
-        bool _stopped;
+	bool _stopped;
 };
--- src/window.hpp
+++ src/window.hpp
@@ -6,30 +6,30 @@
 class Window
 {
 public:
-        class WC
-        {
-        public:
-                static const char* name();
-                static HINSTANCE instance();
+	class WC
+	{
+	public:
+		static const char* name();
+		static HINSTANCE instance();
 
-        private:
-                WC();
-                ~WC();
+	private:
+		WC();
+		~WC();
 
-                static constexpr const char* _name = "loosehead";
-                static WC _wc;
-                HINSTANCE _instance;
-        };
+		static constexpr const char* _name = "loosehead";
+		static WC _wc;
+		HINSTANCE _instance;
+	};
 
 public:
-        Window(int width, int height, const char* title);
-        ~Window();
+	Window(int width, int height, const char* title);
+	~Window();
 
-        void set_title(const char* title);
+	void set_title(const char* title);
 private:
-        HWND _window;
+	HWND _window;
 
-        static LRESULT CALLBACK message_setup(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
-        static LRESULT CALLBACK message_thunk(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
-        LRESULT CALLBACK wnd_proc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+	static LRESULT CALLBACK message_setup(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+	static LRESULT CALLBACK message_thunk(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+	LRESULT CALLBACK wnd_proc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 };
